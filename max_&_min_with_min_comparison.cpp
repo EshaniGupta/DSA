125b19b49c867d82932d9801ff0907aaa1e540f5
Eshani Gupta <87672818+EshaniGupta@users.noreply.github.com>
2022-11-26 00:14:32 +0530

    Update max_&_min_with_min_comparison.cpp

--- max_&_min_with_min_comparison.cpp
+++ max_&_min_with_min_comparison.cpp
@@ -2,12 +2,13 @@
 //IN CASE OF TAKING FIRST 2 AS PAIR AND COMPARING REST OF THE ELEMENTS ON BY once...NO. OF COMPARISONS IS 2*(N-2)+1
 //(optimized solution) IN CASE OF TAKING FIRST 2 AS PAIR AND COMPARING REST OF THE ELEMENTS AS PAIR OF 2 AGAIN IN AN ARRAY..NO. OF COMPARISONS (odd) IS 3*(n-1)/2 and for even is 1+3*(n-2/2)
 #include<iostream>
+using namespace std;
 int main()
 {
   int n;
   cin>>n;
   int a[n];
-  for (int i=0,i<n;i++) cin>>a[i];
+  for (int i=0;i<n;i++) cin>>a[i];
   int max, min;
   int i;
 
@@ -29,15 +30,22 @@ int main()
       i=1;
     }
     while (i<n-1){
-      if (a[i]>a[i+1]){
+      if (a[i]>max){
         max = a[i];
       }
-      if (a[i]<min){
-        min = a[i];
+      if (a[i+1]<min){
+        min = a[i+1];
+      }
+      else{
+          if (a[i+1]>max){
+            max=a[i+1];  
+          }
+          if (a[i]<min){
+            min=  a[i];
       }
     }
     i=i+2;
-  }
+    }
   cout<<"Maximum value = "<<max<<endl;
   cout<<"Minimum value = "<<min<<endl;
 
