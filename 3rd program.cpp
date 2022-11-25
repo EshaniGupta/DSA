4b10d54847103b943675379e34316bc75170705d
Eshani Gupta <87672818+EshaniGupta@users.noreply.github.com>
2022-11-25 23:55:04 +0530

    Add files via upload

---
+++ 3rd program.cpp
@@ -0,0 +1,45 @@
+//Find Max and Min element in array using minimum number of comparisions
+//IN CASE OF TAKING FIRST 2 AS PAIR AND COMPARING REST OF THE ELEMENTS ON BY once...NO. OF COMPARISONS IS 2*(N-2)+1
+//(optimized solution) IN CASE OF TAKING FIRST 2 AS PAIR AND COMPARING REST OF THE ELEMENTS AS PAIR OF 2 AGAIN IN AN ARRAY..NO. OF COMPARISONS (odd) IS 3*(n-1)/2 and for even is 1+3*(n-2/2)
+#include<iostream>
+int main()
+{
+  int n;
+  cin>>n;
+  int a[n];
+  for (int i=0,i<n;i++) cin>>a[i];
+  int max, min;
+  int i;
+
+  if n%2==0{
+    if (a[0]>a[1]){
+      max = a[0];
+      min = a[1];
+    }
+    else{
+      max = a[1];
+      min = a[0];
+    }
+    i=2;
+
+    }
+    else{
+      max = a[0];
+      min = a[0];
+      i=1;
+    }
+    while (i<n-1){
+      if (a[i]>a[i+1]){
+        max = a[i];
+      }
+      if (a[i]<min){
+        min = a[i];
+      }
+    }
+    i=i+2;
+  }
+  cout<<"Maximum value = "<<max<<endl;
+  cout<<"Minimum value = "<<min<<endl;
+
+  return 0;
+}
